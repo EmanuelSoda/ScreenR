724a21add2dfc25af0721858d6611e64c591102e
Emanuel Michele Soda <emanuelsoda@gmail.com>
2021-07-28 20:13:10 +0200

    First commit

---
+++ R/create_Screen_object.R
@@ -0,0 +1,23 @@
+#' Create the Screen Object
+#' @title create_Screen_object
+#' @description Initial function to create the Screen Object.
+#' @param count_table The count table obtained from the read alignment that
+#'                    contains the Barcodes as rows and samples as columns.
+#' @param annotation_table The annotation table containing the information
+#'                        for each Barcode and the association to the
+#'                        corresponding Gene
+#' @param groups A factor containing the experimental design label
+#' @param replicates A factor containing the replicates
+#' @param color_palette a vector containing the color palette
+#'
+#' @return An object containing all the information for the analysis.
+
+create_Screen_object <- function(count_table, annotation_table,
+                                 groups, replicates, color_palette) {
+  setClass("Screen_object",
+           slots = list(count_table = count_table,
+                        annotation_table = annotation_table,
+                        groups = groups,
+                        replicates = replicates,
+                        color_palette =  color_palette))
+}
