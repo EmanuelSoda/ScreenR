366271254b05679bedc0c255513650f0f978910c
Emanuel Soda <emanuelsoda@gmail.com>
2022-05-10 11:42:15 +0200

    other update

---
+++ R/ScreenR-pacakage.R
@@ -0,0 +1,18 @@
+#' Tools for analyzing shRNAs screening
+#'
+#' `ScreenR` implements a series of functions to facilitate inspection,
+#' analysis and visualization of shRNAs screening datasets. `ScreenR`
+#' takes the a count table as input and create the screenr_object to perform
+#' the analysis.
+#'
+#' `ScreenR` anable the user to perform quality control, visual inspection,
+#' dimensionality reduction of the data. It uses statistical methods to find
+#'
+#'
+#' @author Emanuel Michele Soda \email{emanuelsoda@@gmail.com}
+#' @name ScreenR-package
+#' @docType package
+#' @aliases ScreenR ScreenR-package
+#' @keywords internal
+#'
+NULL
--- R/annotation_table.R
+++ R/annotation_table.R
@@ -4,17 +4,23 @@
 #' @keywords datasets
 #' @concept data
 #'
-#' @format A data frame with 5320 rows and 2 columns:
+#' @format A data frame with 5320 rows and 2 columns obtained from a
+#'         loss-of-function genetic screening:
 #' \describe{
 #'   \item{Gene}{It Contains the gene name}
-#'   \item{Barcode}{It contains an ID that identify each barcode, it can be use
-#'   to merge the annotation table with the count table}
+#'   \item{Barcode}{It contains an ID that identify each barcode
+#'                  (it is an unique identifier for an shRNA). I
+#'                  t can be use to merge the annotation table with t
+#'                  he count table}
 #'
 #'   \item{Gene_ID}{It Contains a unique Gene ID}
 #'
-#'   \item{Sequence}{It contains the cDNA sequence of the Barcode}
+#'   \item{Sequence}{It contains the cDNA sequence of the shRNA associated to
+#'                   the barcode}
 #'
-#'   \item{Library}{It contains the library from which the Barcode comes from}
+#'   \item{Library}{It contains the library from which the shRNA come from.
+#'                  In this case is a pooled from
+#'                  \code{\link{https://cellecta.com/}}}
 #' }
 #'
 "annotation_table"
